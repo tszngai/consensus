0ad4449f06bd49a86d162449c81b95428d0494ed
TSZ NGAI FU <tszngaifu@TSZde-MacBook-Pro.local>
2019-04-26 01:25:54 -0700

    added client file and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # consensus
 Consensus algorithms in IoT application
+
+In this work we compare and contrast two consensus protocols, Paxos and Raft, to study how consensus mechanism performs in an IoT setup. Specifically, we build on works from https://github.com/cocagne/multi-paxos-example and https://github.com/bakwc/PySyncObj.
+
+Our experiment setup includes five RPi's as consensus nodes and a Mac workstation as an external client. To rebuild our work, simply clone the repo and run the server Python file on at least three locally connected RPi's. Also run the client file on an external device and make sure it is within the same network as the consensus cluster.
---
+++ Raft/counter_client_loop.py
@@ -0,0 +1,80 @@
+import sys
+import time
+from functools import partial
+sys.path.append("../")
+from pysyncobj import SyncObj, replicated, config
+
+class CountObj(SyncObj):
+
+    def __init__(self, selfNodeAddr, otherNodeAddrs):
+        super(CountObj, self).__init__(selfNodeAddr, otherNodeAddrs, conf=config.SyncObjConf(dynamicMembershipChange=True))
+        self.__counter = 0
+        self.loopCount = 0
+        self.sleep = 0.0001
+
+    @replicated
+    def inc(self):
+        self.__counter += 1
+        return self.__counter
+
+    @replicated
+    def add(self, value):
+        self.__counter += value
+        return self.__counter
+    
+    @replicated
+    def dec(self):
+        self.__counter -= 1
+        return self.__counter
+    
+    @replicated
+    def sub(self, value):
+        self.__counter -= value
+        return self.__counter
+
+    def getCounter(self):
+        return self.__counter
+
+    def status(self):
+        for k, v in self.getStatus().items():
+            print('%s: %s' % (str(k), str(v)))
+
+    def leader(self):
+        print(self.getStatus()['leader'])
+
+    def state(self):
+        print(self.getStatus()['state'])
+
+    def partnerCount(self):
+        print(self.getStatus()['partner_nodes_count'])
+
+    def readonlyCount(self):
+        print(self.getStatus()['readonly_nodes_count'])
+
+    def unknownCount(self):
+        print(self.getStatus()['unknown_connections_count'])
+
+    def logLen(self):
+        print(self.getStatus()['log_len'])
+
+    def uptime(self):
+        print(self.getStatus()['uptime'])
+
+    def sendLoop(self):
+        while True:
+            self.inc()
+            self.loopCount += 1
+            time.sleep(self.sleep)
+
+    def setDelay(self, x):
+        self.sleep = x
+
+    def reset(self):
+        self.sub(self.getCounter())
+        self.loopCount = 0
+
+local = '192.168.1.' + sys.argv[1]
+
+other = ['192.168.1.' + p for p in sys.argv[2:]]
+
+o = CountObj(local, other)
